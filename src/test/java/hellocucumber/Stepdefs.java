5068d8044f48179f4180f68ec6bfd3fa37657e0e
titonoolvida@gmail.com <titonoolvida@gmail.com>
2019-04-01 12:16:32 -0300

    inicio

---
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -0,0 +1,28 @@
+package hellocucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+}
